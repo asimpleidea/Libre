4ad2bd51b1f829254d54524ce1e30a0b121a7112
maschiuma <maschiuma@gmail.com>
2018-04-16 17:54:24 +0200

    Changed package name

--- app/src/main/java/mad24/polito/it/Book.java
+++ app/src/main/java/mad24/polito/it/Book.java
@@ -5,6 +5,29 @@ class Book {
     private String title;
     private String author;
     private String year;
-    //private
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
 
 }
