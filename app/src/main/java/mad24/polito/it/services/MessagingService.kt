b5e2e1702b5dc95e16278bc282870936b247f367
Elis <34626570+SunSince90@users.noreply.github.com>
2018-05-16 20:19:20 +0200

    polished some code on the notifications

--- app/src/main/java/mad24/polito/it/services/MessagingService.kt
+++ app/src/main/java/mad24/polito/it/services/MessagingService.kt
@@ -5,36 +5,17 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.TaskStackBuilder
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.drawable.Drawable
 import android.support.v4.app.NotificationCompat
-import android.util.Log
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.drawable.GlideDrawable
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import mad24.polito.it.R
-import java.lang.Exception
-import javax.sql.DataSource
-import android.graphics.drawable.BitmapDrawable
-import android.transition.Transition
-import com.bumptech.glide.request.animation.GlideAnimation
-import com.bumptech.glide.request.target.SizeReadyCallback
-import com.bumptech.glide.request.target.BaseTarget
-import com.bumptech.glide.request.target.SimpleTarget
 import android.graphics.BitmapFactory
-import android.net.Uri
 import android.os.Build
 import android.support.v4.app.NotificationManagerCompat
-import com.bumptech.glide.Priority
-import com.google.android.gms.tasks.Task
 import com.google.firebase.storage.FirebaseStorage
 import mad24.polito.it.chats.ChatActivity
 import java.net.HttpURLConnection
 import java.net.URI
-import java.net.URL
 
 
 class MessagingService : FirebaseMessagingService()
