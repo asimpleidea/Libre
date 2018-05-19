607acf987deb244e448be7f63a5ce1ec8e4014fb
Elis <34626570+SunSince90@users.noreply.github.com>
2018-05-19 18:09:02 +0200

    refactored chat model

--- app/src/main/java/mad24/polito/it/models/Chat.kt
+++ app/src/main/java/mad24/polito/it/models/Chat.kt
@@ -1,15 +1,16 @@
 package mad24.polito.it.models
 
-class Chat constructor(_chat : String = "" )
+class Chat constructor(_chat : String = "",
+                       _last_message_time : String = "",
+                       _last_message_by : String = "",
+                       _last_message_id : String = "",
+                       _preview : String = "",
+                       _partner_id : String = "")
 {
-    val chat = _chat
-    var last_message : LastMessage? = null
-
-    class LastMessage constructor(_by : String = "", _time : String = "", _preview : String = "", _id : String = "")
-    {
-        val by = _by
-        val time = _time
-        val preview = _preview
-        val id = _id
-    }
+    val chat_id = _chat
+    val last_message_time = _last_message_time
+    val last_message_id = _last_message_id
+    val last_message_by = _last_message_by
+    val partner_id = _partner_id
+    val preview = _preview
 }
